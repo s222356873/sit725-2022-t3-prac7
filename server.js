f19fa706ecd7b695a2458a120663c875bfb66e7a
s222356873 <117885788+s222356873@users.noreply.github.com>
2023-02-16 12:01:40 +1100

    updated

--- server.js
+++ server.js
@@ -10,6 +10,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/projects", projectRoutes);
 
+let io = require('socket.io')(http);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
+
+});
+
+
+//socket test
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
+});
+http.listen(port,()=>{
+  console.log("Listening on port ", port);
+});
+
+
 var port = process.env.port || 3000;
 
 app.listen(port, () => {
